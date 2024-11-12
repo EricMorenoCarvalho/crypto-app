c1646396f9defac8bbbb12177ba5ca33ccd2d3fd
Eric Moreno Carvalho <154634169+EricMorenoCarvalho@users.noreply.github.com>
2024-11-12 20:57:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Home from "./pages/Home"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import ScrollToTop from "./components/ScrollToTop"
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+
+const App = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    let title = 'Crypto Platform'
+    switch (location.pathname) {
+      case '/':
+        title = title + ' | Home'
+        break
+    }
+    document.title = title
+  }, [location.pathname])
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+      <ScrollToTop />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='flex items-center h-screen'>
+            <div className='mx-auto'>
+                Home
+            </div>
+        </div>
+    )
+}
+
+export default Home
